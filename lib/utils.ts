6bfff40a5a159da907fdb3559e45132d90574445
julsweonCodes <julsweon@gmail.com>
2025-07-15 05:51:46 +0900

    build: supabase, session, db 등 세팅, 공통 함수 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDateYYYYMMDD(date: Date): string {
+  return `${date.getFullYear()}${String(date.getMonth() + 1).padStart(2, '0')}${String(date.getDate()).padStart(2, '0')}`;
+}
