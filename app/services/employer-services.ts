7aa36b690b7bd2054b7becc449483a97b6efb86f
julsweonCodes <julsweon@gmail.com>
2025-07-15 05:53:46 +0900

    feat: ADD employer-service dashboard cnts

--- app/services/employer-services.ts
+++ app/services/employer-services.ts
@@ -1,7 +1,10 @@
 // 📁 services/employer-service.ts
-import { supabase } from '@/lib/client/supabase'
+import { supabaseClient } from '@/utils/supabase/client'
 import { EmployerProfilePayload } from '@/types/employer'
+import { prisma } from "@/app/lib/prisma/prisma-singleton";
+import {formatDateYYYYMMDD} from "@/lib/utils";
 
+// uploading images to supabase storage
 export async function uploadEmployerImages(photos: File[], userId: number): Promise<string[]> {
   for (const photo in photos) {
   }
@@ -11,7 +14,7 @@ export async function uploadEmployerImages(photos: File[], userId: number): Prom
     const fileExt = photo.name.split('.').pop()
     const filePath = `${Date.now()}-${userId}-${Math.random().toString(36).substring(2)}.${fileExt}`;
 
-    const { error: uploadError } = await supabase
+    const { error: uploadError } = await supabaseClient
       .storage
       .from('job-about')
       .upload("biz-loc-photo/".concat(filePath), photo);
@@ -24,21 +27,21 @@ export async function uploadEmployerImages(photos: File[], userId: number): Prom
   return urls;
 }
 
-export async function deleteEmployerImages(imageUrls: string[]) {
-  const filePaths = imageUrls.map((url) => {
-    const parts = url.split('/job-about/')
-    return parts[1] ? `job-about/${parts[1]}` : ''
-  }).filter(Boolean)
+// deleting images from supabase storage
+export async function deleteSingleEmployerImage(url: string) {
+  const parts = url.split('/job-about/');
+  const path = parts[1] ? `job-about/${parts[1]}` : '' ;
+  if (!path) return;
 
-  if (filePaths.length === 0) return
-
-  const { error } = await supabase.storage
+  const { error } = await supabaseClient.storage
     .from('job-about')
-    .remove(filePaths)
+    .remove([path])
+  ;
 
-  if (error) throw error
+  if (error) throw error;
 }
 
+// POST - Create business loc
 export async function saveEmployerProfile(payload: EmployerProfilePayload) {
   const safePayload = {
     ...payload,
@@ -46,32 +49,109 @@ export async function saveEmployerProfile(payload: EmployerProfilePayload) {
     user_id : Number(payload.user_id?? 1),
   };
   console.log(safePayload);
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from('business_loc')
-    .insert([safePayload]);
+    .insert([safePayload])
+  ;
 
-  if (error) throw error
-  return data
+  if (error) throw error;
+  return data;
 }
 
+// PUT - Update business loc
 export async function updateEmployerProfile(id: number, payload: EmployerProfilePayload) {
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from('business_loc')
     .update(payload)
-    .eq('id', id)
+    .eq('id', id);
 
-  if (error) throw error
-  return data
+  if (error) throw error;
+  return data;
 }
 
-export async function deleteSingleEmployerImage(url: string) {
-  const parts = url.split('/job-about/')
-  const path = parts[1] ? `job-about/${parts[1]}` : ''
-  if (!path) return
+// GET - Select cnt values for employer dashboard
+export async function getActiveJobPosts(userId: number): Promise<number> {
+  const currDate = new Date();
+  const currDateStr = formatDateYYYYMMDD(currDate)
+  const count = await prisma.job_posts.count({
+    where: {
+      created_at: {
+        lte: currDate,
+      },
+      deadline: {
+        gte: currDateStr,
+      },
+      user_id: userId,
+      status: "PUBLISHED",
+    },
+  });
 
-  const { error } = await supabase.storage
-    .from('job-about')
-    .remove([path])
+  return count;
+}
 
-  if (error) throw error
+export async function getStatusUpdateCnt(userId: number): Promise<number> {
+  const currDate = new Date();
+  const tomorrow = new Date(currDate);
+  tomorrow.setDate(currDate.getDate()+1);
+  const tomorrowDate = formatDateYYYYMMDD(tomorrow);
+  console.log(currDate, tomorrow);
+
+  const urgentJobPosts = await prisma.job_posts.findMany({
+    where: {
+      created_at: {
+        lte:  currDate,
+      },
+      deadline: tomorrowDate,
+      user_id: userId,
+      status: "PUBLISHED",
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const validJobPostIds = urgentJobPosts.map((post => post.id));
+
+  // 2. count applications
+  const applicationCnt = await prisma.applications.count({
+    where: {
+      job_post_id: {
+        in: validJobPostIds,
+      },
+    },
+  });
+
+  return applicationCnt;
+}
+
+export async function getAllApplicationsCnt(userId: number): Promise<number> {
+  const currDate = new Date();
+  const currDateStr = formatDateYYYYMMDD(currDate);
+
+  const allJobPosts = await prisma.job_posts.findMany({
+    where: {
+      created_at: {
+        lte:  currDate,
+      },
+      deadline: currDateStr,
+      user_id: userId,
+      status: "PUBLISHED",
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  const validJobPostIds = allJobPosts.map((post => post.id));
+
+  // 2. count applications
+  const applicationCnt = await prisma.applications.count({
+    where: {
+      job_post_id: {
+        in: validJobPostIds,
+      },
+    },
+  });
+
+  return applicationCnt;
 }
