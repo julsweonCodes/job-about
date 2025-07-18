717c0f2c7634634fa8fd61af2a0fa7b52dde8479
julsweonCodes <julsweon@gmail.com>
2025-07-19 06:19:28 +0900

    feat: add Gemini test api

--- app/api/employer/post/create/route.ts
+++ app/api/employer/post/create/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getUserIdFromSession } from "@/utils/auth";
 import { createJobPost, getBusinessLocId } from "@/app/services/job-post-services";
+import { geminiTest } from "@/app/services/gemini-services";
 import { JobPostPayload} from "@/types/employer";
 
 // create job post
@@ -14,5 +15,15 @@ export async function POST (request: NextRequest) {
   } catch (error) {
     console.error("❌ error on creating job post:", error);
     return NextResponse.json({ error: "Failed to create job post" }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const result = await geminiTest();
+    return NextResponse.json( {data: result}, {status: 200});
+  } catch(error) {
+    console.log("gemini error");
+    return NextResponse.json({error: "gemini api test fail"}, { status: 500});
   }
 }
