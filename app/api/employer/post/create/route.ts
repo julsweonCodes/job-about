576f287c5087250ecc0a24ae8ea6b6afd8aa3a7e
julsweonCodes <julsweon@gmail.com>
2025-08-08 17:29:57 +0900

    result merge conflict

--- app/api/employer/post/create/route.ts
+++ app/api/employer/post/create/route.ts
@@ -1,18 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server';
 import { getUserIdFromSession } from "@/utils/auth";
-import { createJobPost } from "@/app/services/job-post-services";
+import { createJobPost, getBusinessLocId } from "@/app/services/job-post-services";
 import { geminiTest } from "@/app/services/gemini-services";
-import { JobPostPayload } from "@/types/employer";
+import { JobPostPayload} from "@/types/employer";
+import { getSession } from "@/utils/withSession";
 import { successResponse } from "@/app/lib/server/commonResponse";
-import { setCache } from "@/utils/cache";
+import { setCache, getCache } from "@/utils/cache";
+import { setupFsCheck } from "next/dist/server/lib/router-utils/filesystem";
 
 // create job post
 
-export async function POST(request: NextRequest) {
+export async function POST (request: NextRequest) {
   const body = (await request.json()) as JobPostPayload;
+  const userId = await getUserIdFromSession();
+  let result : any = null;
   let geminiRes: any = null;
   try {
-    const userId = await getUserIdFromSession();
     const createPostRes = await createJobPost(body, userId);
     if (body.useAI) {
       geminiRes = await geminiTest(body);
@@ -23,10 +26,8 @@ export async function POST(request: NextRequest) {
       {
         id: Number(createPostRes.id),
         description: createPostRes.description,
-        geminiRes: geminiRes,
-      },
-      200
-    );
+        geminiRes: geminiRes
+      }, 200);
   } catch (error) {
     console.error("❌ error on creating job post:", error);
     return NextResponse.json({ error: "Failed to create job post" }, { status: 500 });
